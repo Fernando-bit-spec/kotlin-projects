b1e918526455c5ebbdd9208b591e944a7bce23ea
Fernando <fdsantosj15@gmail.com>
2026-02-19 12:13:55 -0300

    implementação de funções

--- app/src/main/java/com/example/caraoucoroa/MainActivity.kt
+++ app/src/main/java/com/example/caraoucoroa/MainActivity.kt
@@ -4,28 +4,39 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.LocalMinimumInteractiveComponentEnforcement
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.text.rememberTextMeasurer
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.caraoucoroa.ui.theme.CaraOuCoroaTheme
+import kotlin.random.Random
 
 class MainActivity : ComponentActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {git
+    override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            CaraOuCoroaTheme {
+            CaraOuCoroaTheme() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    CaraOuCoroa(modifier = Modifier.padding(paddingValues = innerPadding))
                 }
             }
+
+
         }
     }
 }
@@ -45,4 +56,28 @@ fun GreetingPreview() {
     CaraOuCoroaTheme {
         Greeting("Android")
     }
+
+
+}
+
+@Composable
+fun CaraOuCoroa(modifier: Modifier = Modifier) {
+        var resultado by remember { mutableStateOf(value = "") }
+    Column(
+        modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "Jogo cara ou coroa")
+        Spacer(Modifier.height(height = 16.dp))
+        Button(onClick = {
+            val resultado = Random().nextBoolean()
+        }) {Text(text= "jogar")
+    }
+
+}
+Text(
+text = "Cara ou coroa"
+)
+
 }
